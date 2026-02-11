6b34386afc09b688981d0666a02e34633b72ea63
denyskov11 <bsdensh9371@gmail.com>
2026-02-11 20:25:04 +0200

    redisgned web and added logo to the web,

--- online_shop/app/routes.py
+++ online_shop/app/routes.py
@@ -5,7 +5,8 @@ bp=Blueprint('routes', __name__) #creating blueprint
 #adding path to blueprint
 @bp.route('/')
 def index():
-    return render_template('index.html') #when user goes to main page shows html web page
+    products=Product.query.all()
+    return render_template('index.html', products=products)
 @bp.route('/products')
 def product():
     products=Product.query.all()
@@ -27,3 +28,32 @@ def add_product():
         flash('Product added!')
         return redirect(url_for('routes.product')) #back to the product list page
      return render_template('product_form.html', action='Add', product=None) #if user did not submit form, showing html form
+ 
+ 
+ 
+@bp.route('/delete/<int:product_id>', methods=['POST']) #getting product ID that needs to be updated
+def delete_product(product_id):
+    product = Product.query.get_or_404(product_id) #searching product in the database, if it does not exist, return error
+    db.session.delete(product) #confirming that product need to be deleted
+    db.session.commit()
+    flash('Product deleted!')
+    return redirect(url_for('routes.product')) #going back to the product list page
+
+@bp.route('/update/<int:product_id>', methods=['GET', 'POST']) #getting product ID that needs to be updated
+def update_product(product_id):
+    product = Product.query.get_or_404(product_id) #searching product in the database, if it does not exist, return error
+    if request.method == 'POST': #if user clicked update button in the form
+        #getting previous data
+        product.name = request.form['name'] 
+        product.price = float(request.form['price'])
+        product.description = request.form['description'] 
+        product.stock = request.form['stock']
+        product.is_active = request.form.get('is_active') == 'on'
+        product.category = request.form['category']
+        product.rating = request.form['rating']
+        product.sale = request.form.get('sale') == 'on'
+        
+        db.session.commit() #confirming update of the product
+        flash('Product updated!')
+        return redirect(url_for('routes.product')) #going back to the product list page
+    return render_template('product_form.html', action='update', product=product)  #if get method showing update form
