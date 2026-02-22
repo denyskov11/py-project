a2741e39c8a6e2e15f9c1a2314477bf3069db588
denyskov11 <bsdensh9371@gmail.com>
2026-02-22 11:12:46 +0200

    preparation for running on server

--- online_shop/app/__init__.py
+++ online_shop/app/__init__.py
@@ -11,13 +11,20 @@ def create_app():
     with open(config_path) as f:
         config = json.load(f)
     app.config.update(config) #add all config information to FLASK
-    # Set up SQLAlchemy with absolute DB path
-    db_path = os.path.abspath(os.path.join(base_dir, config['DB_PATH']))
-    # Ensure the DB directory exists so SQLite can create the file
-    db_dir = os.path.dirname(db_path)
-    if not os.path.exists(db_dir): 
-        os.makedirs(db_dir, exist_ok=True) #if theres no folder creating one
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}" #where is our db
+   # Use DATABASE_URL env variable for Postgres if set, else fallback to SQLite
+    database_url = os.environ.get('DATABASE_URL')
+    db_path = None
+    if database_url:
+        print ("DB URL is set. Postgres DB")
+        app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    else:
+        print ("DB URL is NOT set. SQLite DB")
+        db_path = os.path.abspath(os.path.join(base_dir, config['DB_PATH']))
+        db_dir = os.path.dirname(db_path)
+        if not os.path.exists(db_dir):
+            os.makedirs(db_dir, exist_ok=True)
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+ #where is our db
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #turning off anoying messages
     #connecting models
     from .models import db #importing db from models
